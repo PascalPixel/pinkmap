28990c373c27f6b30677ae69157afed3fe78bf6d
Lewis Horwood <horwood.lewis@gmail.com>
2018-12-01 14:29:57 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+        Pinkmap
+          <nav>
+            <a href="#" className="nav-item">Map</a>
+            <a href="#" className="nav-item">Report</a>
+            <a href="#" className="nav-item">About</a>
+          </nav>
+        </header>
+        <main>
+          
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
