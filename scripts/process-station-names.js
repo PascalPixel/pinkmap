0e9f209a13b838e6f8124b054b8261f06e5441db
Benjamin <trade@sha.company>
2018-12-01 18:47:15 +0900

    added data and process-station-names

---
+++ scripts/process-station-names.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const _ = require('lodash');
+const stationJson = JSON.parse(fs.readFileSync('../json/seoul-stations.json', 'utf8'));
+
+console.log('Data size', stationJson.DATA.length);
+const filteredData = stationJson.DATA
+		.filter(d => d.station_nm_han !== null)
+		.filter(d => d.station_nm_eng !== null);
+console.log('Data size with names', filteredData.length);
+const noDupData = Object.values(stationJson.DATA.reduce((d, e) => {
+	d[e.station_nm_eng] = e;
+	return d;
+}, {}));
+console.log('Data size no dup', noDupData.length);
+
+console.log('Total stations id:', [...(new Set(stationJson.DATA.map(d => d.station_cd)))].length);
+console.log('Total stations english names:', [...(new Set(stationJson.DATA.map(d => d.station_nm_eng)))].length);
+console.log('Total stations korean names:', [...(new Set(stationJson.DATA.map(d => d.station_nm)))].length);
+console.log('Total stations chinese names:', [...(new Set(stationJson.DATA.map(d => d.station_nm_han)))].length);
+
+const mappedData = noDupData.map(({station_nm_han, station_nm_eng, line_num}) => {
+	return {
+		lineNum: line_num,
+		en: station_nm_eng,
+		kr: station_nm_han,
+	};
+});
+
+const groupedByLine = _.groupBy(noDupData, 'line_num');
+console.log('All lines:', Object.keys(groupedByLine).map(k => 'Line ' + k).join(', '));
+Object.keys(groupedByLine).forEach((lineKey) => {
+	console.log('Line ' + lineKey + ', num. of stations', groupedByLine[lineKey].length);
+});
+
+const finishData = _.groupBy(mappedData, 'lineNum');
+
+fs.writeFileSync('../json/seoul-stations-reduced.json', JSON.stringify(finishData, null, '\t'), 'utf8');
