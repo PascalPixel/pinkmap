54af069971ffe2237a9bcf9c7652e6884f0464fd
Superpencil <pascal@superpencil.com>
2018-12-01 17:16:42 +0900

    Map Data

---
+++ src/Map.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Map.css';
+import STATIONS from './STATIONS';
+
+class Map extends Component {
+  render() {
+    return (
+      <div className="Map">
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default Map;
