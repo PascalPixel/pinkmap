233f5b01de81f0236cbe8cb54f0c48641f2c18a9
Superpencil <pascal@superpencil.com>
2018-12-01 19:28:14 +0900

    MAp

--- src/Map.js
+++ src/Map.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import './Map.css';
+import BaseMap from './BaseMap'
 import STATIONS from './STATIONS';
 
 class Map extends Component {
   render() {
     return (
       <div className="Map">
-        <h1>Hello World</h1>
+        <div className="list">
+          {STATIONS.map(station =>
+            <div className="station">
+              <div>
+                {station.lines.map(line => 
+                  <span style={{ color: line.stroke }}>◉</span>
+                )}
+                &nbsp;
+                {station.names.map(name => 
+                  <span>{name.name}&nbsp;</span>
+                )}
+              </div>
+            </div>
+          )}
+        </div>
+        <div className="map">
+          <BaseMap />
+        </div>
       </div>
     );
   }
